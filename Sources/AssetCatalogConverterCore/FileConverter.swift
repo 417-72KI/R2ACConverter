c6feada1f97e953cf16931e016aaff452fe16213
417-72KI <417.72ki@gmail.com>
2023-09-22 00:52:52 +0900

    use `SyntaxRewriter` instead

--- Sources/AssetCatalogConverterCore/FileConverter.swift
+++ Sources/AssetCatalogConverterCore/FileConverter.swift
@@ -7,98 +7,43 @@ enum FileConverter {}
 extension FileConverter {
     static func convert(_ input: String) throws -> String {
         let source = Parser.parse(source: input)
-
-        for codeBlock in source.statements {
-            let item = codeBlock.item
-            if let decl = item.as(VariableDeclSyntax.self) {
-                try convertVariable(decl)
-                continue
-            }
-            if let decl = item.as(ClassDeclSyntax.self) {
-                try convertClass(decl)
-                continue
-            }
-            if let decl = item.as(StructDeclSyntax.self) {
-                try convertStruct(decl)
-                continue
-            }
-            if let decl = item.as(EnumDeclSyntax.self) {
-                try convertEnum(decl)
-                continue
-            }
-            if let decl = item.as(ExtensionDeclSyntax.self) {
-                try convertExtension(decl)
-                continue
-            }
-        }
-        // TODO: not implemented
-        return input
+        let converted = ResourceRewriter(viewMode: .all)
+            .visit(source)
+        return converted.description
     }
 }
 
-private extension FileConverter {
-    static func convertVariable(_ decl: VariableDeclSyntax) throws {
-        for binding in decl.bindings {
-            print(binding)
-            if let expr = binding.initializer?.value.as(FunctionCallExprSyntax.self) {
-                try convertFunctionCall(expr)
-                continue
-            }
-            if let expr = binding.initializer?.value.as(ForceUnwrapExprSyntax.self) {
-                if let expr = expr.expression.as(FunctionCallExprSyntax.self) {
-                    try convertFunctionCall(expr)
-                }
-                continue
-            }
-        }
-    }
-
-    static func convertFunctionCall(_ expr: FunctionCallExprSyntax) throws {
-        guard let member = expr.calledExpression.as(MemberAccessExprSyntax.self),
-              let base = member.base?.as(MemberAccessExprSyntax.self),
-              base.base?.as(DeclReferenceExprSyntax.self)?.baseName.text == "R",
-              base.declName.baseName.text == "image" else { return }
-        let imageName = member.declName.baseName.text.camelized
-        let newSyntax = FunctionCallExprSyntax(
-            leadingTrivia: expr.leadingTrivia,
-            calledExpression: DeclReferenceExprSyntax(
-                baseName: .identifier("UIImage")
-            ),
-            leftParen: expr.leftParen,
-            arguments: [
-                LabeledExprSyntax(
-                    label: .identifier("resource"),
-                    colon: .colonToken(trailingTrivia: .space),
-                    expression: MemberAccessExprSyntax(
-                        period: .periodToken(),
-                        name: .identifier(imageName)
-                    )
+private class ResourceRewriter: SyntaxRewriter {
+    override func visit(_ node: FunctionCallExprSyntax) -> ExprSyntax {
+        if let member = node.calledExpression.as(MemberAccessExprSyntax.self),
+           let base = member.base?.as(MemberAccessExprSyntax.self),
+           base.base?.as(DeclReferenceExprSyntax.self)?.baseName.text == "R",
+           base.declName.baseName.text == "image" {
+            let imageName = member.declName.baseName.text.camelized
+            return super.visit(
+                FunctionCallExprSyntax(
+                    leadingTrivia: node.leadingTrivia,
+                    calledExpression: DeclReferenceExprSyntax(
+                        baseName: .identifier("UIImage")
+                    ),
+                    leftParen: node.leftParen,
+                    arguments: [
+                        LabeledExprSyntax(
+                            label: .identifier("resource"),
+                            colon: .colonToken(trailingTrivia: .space),
+                            expression: MemberAccessExprSyntax(
+                                period: .periodToken(),
+                                name: .identifier(imageName)
+                            )
+                        )
+                    ],
+                    rightParen: node.rightParen,
+                    trailingClosure: node.trailingClosure,
+                    additionalTrailingClosures: node.additionalTrailingClosures,
+                    trailingTrivia: node.trailingTrivia
                 )
-            ],
-            rightParen: expr.rightParen,
-            trailingClosure: expr.trailingClosure,
-            additionalTrailingClosures: expr.additionalTrailingClosures,
-            trailingTrivia: expr.trailingTrivia
-        )
-        print(newSyntax.description)
-        print(newSyntax)
-    }
-}
-
-private extension FileConverter {
-    static func convertClass(_ decl: ClassDeclSyntax) throws {
-        print(decl.classKeyword)
-    }
-
-    static func convertStruct(_ decl: StructDeclSyntax) throws {
-        print(decl.structKeyword)
-    }
-
-    static func convertEnum(_ decl: EnumDeclSyntax) throws {
-        print(decl.enumKeyword)
-    }
-
-    static func convertExtension(_ decl: ExtensionDeclSyntax) throws {
-        print(decl.extensionKeyword)
+            )
+        }
+        return super.visit(node)
     }
 }
