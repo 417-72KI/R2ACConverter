45ce9a1770a50b50b50d827afef1e766e1efb102
417-72KI <417.72ki@gmail.com>
2023-09-22 00:55:43 +0900

    add converting `R.color`

--- Sources/AssetCatalogConverterCore/FileConverter.swift
+++ Sources/AssetCatalogConverterCore/FileConverter.swift
@@ -17,33 +17,66 @@ private class ResourceRewriter: SyntaxRewriter {
     override func visit(_ node: FunctionCallExprSyntax) -> ExprSyntax {
         if let member = node.calledExpression.as(MemberAccessExprSyntax.self),
            let base = member.base?.as(MemberAccessExprSyntax.self),
-           base.base?.as(DeclReferenceExprSyntax.self)?.baseName.text == "R",
-           base.declName.baseName.text == "image" {
-            let imageName = member.declName.baseName.text.camelized
-            return super.visit(
-                FunctionCallExprSyntax(
-                    leadingTrivia: node.leadingTrivia,
-                    calledExpression: DeclReferenceExprSyntax(
-                        baseName: .identifier("UIImage")
-                    ),
-                    leftParen: node.leftParen,
-                    arguments: [
-                        LabeledExprSyntax(
-                            label: .identifier("resource"),
-                            colon: .colonToken(trailingTrivia: .space),
-                            expression: MemberAccessExprSyntax(
-                                period: .periodToken(),
-                                name: .identifier(imageName)
+           base.base?.as(DeclReferenceExprSyntax.self)?.baseName.text == "R" {
+            switch base.declName.baseName.text {
+            case "image":
+                let imageName = member.declName.baseName.text.camelized
+                return super.visit(
+                    FunctionCallExprSyntax(
+                        leadingTrivia: node.leadingTrivia,
+                        calledExpression: DeclReferenceExprSyntax(
+                            baseName: .identifier("UIImage")
+                        ),
+                        leftParen: node.leftParen,
+                        arguments: [
+                            LabeledExprSyntax(
+                                label: .identifier("resource"),
+                                colon: .colonToken(trailingTrivia: .space),
+                                expression: MemberAccessExprSyntax(
+                                    period: .periodToken(),
+                                    name: .identifier(imageName)
+                                )
                             )
-                        )
-                    ],
-                    rightParen: node.rightParen,
-                    trailingClosure: node.trailingClosure,
-                    additionalTrailingClosures: node.additionalTrailingClosures,
-                    trailingTrivia: node.trailingTrivia
+                        ],
+                        rightParen: node.rightParen,
+                        trailingClosure: node.trailingClosure,
+                        additionalTrailingClosures: node.additionalTrailingClosures,
+                        trailingTrivia: node.trailingTrivia
+                    )
                 )
-            )
+            case "color":
+                let colorName = member.declName.baseName.text.camelized
+                return super.visit(
+                    FunctionCallExprSyntax(
+                        leadingTrivia: node.leadingTrivia,
+                        calledExpression: DeclReferenceExprSyntax(
+                            baseName: .identifier("UIColor")
+                        ),
+                        leftParen: node.leftParen,
+                        arguments: [
+                            LabeledExprSyntax(
+                                label: .identifier("resource"),
+                                colon: .colonToken(trailingTrivia: .space),
+                                expression: MemberAccessExprSyntax(
+                                    period: .periodToken(),
+                                    name: .identifier(colorName)
+                                )
+                            )
+                        ],
+                        rightParen: node.rightParen,
+                        trailingClosure: node.trailingClosure,
+                        additionalTrailingClosures: node.additionalTrailingClosures,
+                        trailingTrivia: node.trailingTrivia
+                    )
+                )
+            default: break
+            }
         }
         return super.visit(node)
+    }
+
+    override func visit(_ token: ForceUnwrapExprSyntax) -> ExprSyntax {
+        print(token)
+        return super.visit(token)
     }
 }
