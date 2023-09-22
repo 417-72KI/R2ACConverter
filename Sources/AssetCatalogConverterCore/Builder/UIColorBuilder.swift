4a3c15a5600b6bc6992c83146470accb12a78604
417-72KI <417.72ki@gmail.com>
2023-09-22 12:50:20 +0900

    extract common struct hierarchy

--- Sources/AssetCatalogConverterCore/Builder/UIColorBuilder.swift
+++ Sources/AssetCatalogConverterCore/Builder/UIColorBuilder.swift
@@ -1,32 +1,13 @@
 import Foundation
 import SwiftSyntax
 
-struct UIColorBuilder: SyntaxBuilder {
+struct UIColorBuilder: ResourceBuilder {
     var colorName: String
     var leadingTrivia: Trivia?
     var trailingTrivia: Trivia?
+}
 
-    func build() -> FunctionCallExprSyntax {
-        FunctionCallExprSyntax(
-            leadingTrivia: leadingTrivia,
-            calledExpression: DeclReferenceExprSyntax(
-                baseName: .identifier("UIColor")
-            ),
-            leftParen: .leftParenToken(),
-            arguments: [
-                LabeledExprSyntax(
-                    label: .identifier("resource"),
-                    colon: .colonToken(trailingTrivia: .space),
-                    expression: MemberAccessExprSyntax(
-                        period: .periodToken(),
-                        name: .identifier(colorName)
-                    )
-                )
-            ],
-            rightParen: .rightParenToken(),
-            trailingClosure: nil,
-            additionalTrailingClosures: [],
-            trailingTrivia: trailingTrivia
-        )
-    }
+extension UIColorBuilder {
+    var typeName: String { "UIColor" }
+    var value: String { colorName }
 }
--- Sources/AssetCatalogConverterCore/Builder/UIImageBuilder.swift
+++ Sources/AssetCatalogConverterCore/Builder/UIImageBuilder.swift
@@ -1,32 +1,13 @@
 import Foundation
 import SwiftSyntax
 
-struct UIImageBuilder: SyntaxBuilder {
+struct UIImageBuilder: ResourceBuilder {
     var imageName: String
     var leadingTrivia: Trivia?
     var trailingTrivia: Trivia?
+}
 
-    func build() -> FunctionCallExprSyntax {
-        FunctionCallExprSyntax(
-            leadingTrivia: leadingTrivia,
-            calledExpression: DeclReferenceExprSyntax(
-                baseName: .identifier("UIImage")
-            ),
-            leftParen: .leftParenToken(),
-            arguments: [
-                LabeledExprSyntax(
-                    label: .identifier("resource"),
-                    colon: .colonToken(trailingTrivia: .space),
-                    expression: MemberAccessExprSyntax(
-                        period: .periodToken(),
-                        name: .identifier(imageName)
-                    )
-                )
-            ],
-            rightParen: .rightParenToken(),
-            trailingClosure: nil,
-            additionalTrailingClosures: [],
-            trailingTrivia: trailingTrivia
-        )
-    }
+extension UIImageBuilder {
+    var typeName: String { "UIImage" }
+    var value: String { imageName }
 }
