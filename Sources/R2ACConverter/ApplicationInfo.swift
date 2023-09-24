4376abd1083b82d4420a0d59e90b125206775f3a
417-72KI <417.72ki@gmail.com>
2023-09-25 00:04:46 +0900

    Bump version to 0.3.0

--- Sources/R2ACConverter/ApplicationInfo.swift
+++ Sources/R2ACConverter/ApplicationInfo.swift
@@ -3,5 +3,5 @@ import Foundation
 public enum ApplicationInfo {
     public static let author = "417-72KI"
     public static let name = "R2ACConverter"
-    public static let version = "0.2.0"
+    public static let version = "0.3.0"
 }
