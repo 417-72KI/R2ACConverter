11f47774d77bcb34f7a30ec304e03ee25d90db4f
417-72KI <417.72ki@gmail.com>
2023-09-26 01:30:08 +0900

    refactor to use only `◯◯Rewriter`

--- Sources/R2ACConverterCore/FileConverter.swift
+++ Sources/R2ACConverterCore/FileConverter.swift
@@ -16,32 +16,23 @@ extension FileConverter {
 private extension FileConverter {
     final class ResourceRewriter: SyntaxRewriter {
         override func visit(_ node: FunctionCallExprSyntax) -> ExprSyntax {
-            // for UIKit
-            if let member = node.calledExpression.as(MemberAccessExprSyntax.self),
-               let base = member.base?.as(MemberAccessExprSyntax.self),
-               base.base?.as(DeclReferenceExprSyntax.self)?.baseName.text == "R" {
-                let reader = RswiftResourceReader(viewMode: viewMode)
-                switch reader.detectResource(from: base) {
-                case let .image(imageName):
-                    let builder = UIImageBuilder(
-                        imageName: imageName.camelized,
-                        leadingTrivia: node.leadingTrivia,
-                        trailingTrivia: node.trailingTrivia
-                    )
-                    return ExprSyntax(builder.build())
-                case let .color(colorName):
-                    let builder = UIColorBuilder(
-                        colorName: colorName.camelized,
-                        leadingTrivia: node.leadingTrivia,
-                        trailingTrivia: node.trailingTrivia
-                    )
-                    return ExprSyntax(builder.build())
-                default: break
-                }
+            if let expr = RswiftResourceRewriter(viewMode: viewMode).visit(node)
+                .as(FunctionCallExprSyntax.self),
+               expr != node {
+                return visit(expr)
             }
-            // for SwiftUI
             if let decl = node.calledExpression.as(DeclReferenceExprSyntax.self) {
                 switch decl.baseName.text {
+                case "UIImage":
+                    return ExprSyntax(
+                        UIImageRewriter(viewMode: viewMode)
+                            .visit(node)
+                    )
+                case "UIColor":
+                    return ExprSyntax(
+                        UIColorRewriter(viewMode: viewMode)
+                            .visit(node)
+                    )
                 case "Image":
                     return ExprSyntax(
                         SwiftUIImageRewriter(viewMode: viewMode)
