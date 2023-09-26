ef363af795363f2e55ac6e824d64f65cae91f10e
417-72KI <417.72ki@gmail.com>
2023-09-26 20:08:17 +0900

    fix sample

--- SampleApp/R2ACConverterSample/Scene/SwiftUIView.swift
+++ SampleApp/R2ACConverterSample/Scene/SwiftUIView.swift
@@ -9,15 +9,15 @@ struct SwiftUIView: View {
                     Image(.fooBar1)
                 }.background(Color(uiColor: R.color.blue()!))
                 HStack {
-                    Image(R.image.foo_bar_1_2)
+                    Image(uiImage: R.image.foo_bar_1_2()!)
                     Image(.fooBar12)
                 }.background(Color(R.color.blue_1_2))
                 HStack {
-                    Image(R.image.foo_bar_2)
+                    Image(uiImage: UIImage(resource: R.image.foo_bar_2)!)
                     Image(.fooBar2)
                 }.background { Color(uiColor: R.color.blue()!) }
                 HStack {
-                    Image(R.image.foo_bar_baz)
+                    Image(uiImage: .init(resource: R.image.foo_bar_baz)!)
                     Image(.fooBarBaz)
                 }.background {
                     Color(R.color.blue1_2)
